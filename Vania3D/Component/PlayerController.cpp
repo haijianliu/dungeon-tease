909707f76cce5ca3bf18a3cba409c2d10143243c
HaijianLiu <haijanliu@gmail.com>
2017-12-08 16:35:36 +0900

    tunning player controller

--- Vania3D/Component/PlayerController.cpp
+++ Vania3D/Component/PlayerController.cpp
@@ -33,20 +33,14 @@ void PlayerController::update() {
 	if (game->time->currentTime - this->lastAttack > 3.0) {
 		if (abs(axisLS.x) > 0.6 || abs(axisLS.z) > 0.6) {
 			direction = worldToCamera * game->input->getNormalLS();
-			axisLS = worldToCamera * axisLS;
-
-			playerTransform->position.x += 4 * axisLS.x * game->time->deltaTime;
-			playerTransform->position.z += 4 * axisLS.z * game->time->deltaTime;
-
+			playerTransform->position.x += 4 * direction.x * game->time->deltaTime;
+			playerTransform->position.z += 4 * direction.z * game->time->deltaTime;
 			this->animation = 3;
 		}
 		else if (abs(axisLS.x) > 0.1 || abs(axisLS.z) > 0.1){
 			direction = worldToCamera * game->input->getNormalLS();
-			axisLS = worldToCamera * axisLS;
-
-			playerTransform->position.x += 2 * axisLS.x * game->time->deltaTime;
-			playerTransform->position.z += 2 * axisLS.z * game->time->deltaTime;
-
+			playerTransform->position.x += direction.x * game->time->deltaTime;
+			playerTransform->position.z += direction.z * game->time->deltaTime;
 			this->animation = 2;
 		}
 		else {
@@ -58,7 +52,7 @@ void PlayerController::update() {
 		this->animation = 4;
 		this->lastAttack = game->time->currentTime;
 	}
-	
+
 	if (game->input->getJoystickPress(JOY_TRIANGLE)) {
 		playerTransform->position.y += 4 * game->time->deltaTime;
 	}
@@ -68,7 +62,7 @@ void PlayerController::update() {
 
 	playerTransform->rotate(direction, 2 * PI * game->time->deltaTime);
 
-    MeshRenderer* playerMeshRenderer =  this->gameObject->getComponent<MeshRenderer>();
-    if (playerMeshRenderer)
-        playerMeshRenderer->model->updatePose(this->animation, game->time->currentTime);
+	MeshRenderer* playerMeshRenderer =  this->gameObject->getComponent<MeshRenderer>();
+	if (playerMeshRenderer)
+		playerMeshRenderer->model->updatePose(this->animation, game->time->currentTime);
 }
